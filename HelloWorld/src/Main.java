c9d1ed8b2ecbe28677411e59fa4a86d93848f7bd
PuffinKwadraat <sv.kruik@gmail.com>
2022-09-06 13:10:16 +0200

    Init
    With finished projects 1, and nearly done 2.

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Hello world test programma.
+ * @author Stefan Kruik
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Songtekst/src/Main.java
@@ -0,0 +1,57 @@
+/**
+ * Dit programma print de lyrics van een lied.
+ * @author Stefan Kruik
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Titel
+        System.out.println("Chelsea Cutler - Your Shirt\n");
+        // Verse 1
+        System.out.println("This time it don’t feel like you want it all\n" +
+                "Did I do something wrong to make you go?\n" +
+                "Said I would know when it's all good to fall\n" +
+                "Now I just need you here to hold me close\n");
+        // Pre-Chorus
+        System.out.println("Cause I don't want another heart to fill the holes\n" +
+                "That you left under my covers for me to grow cold\n" +
+                "I don't wanna miss somebody who's not missing me\n" +
+                "But the city feels so lonely when you leave\n");
+        // Chorus
+        System.out.println("My room, where I want you\n" +
+                "Tangled ease in my sheets\n" +
+                "In your shirt that I got you\n" +
+                "Half asleep, holding onto me\n" +
+                "My bed where you left me\n" +
+                "Tangled up, now it's noon\n" +
+                "In your shirt that you gave me\n" +
+                "Half asleep, wishing I still had you\n");
+        // Post-Chorus
+        System.out.println("Half asleep, wishing I still had you\n");
+        // Verse 2
+        System.out.println("This time you said, you’re playing games again\n" +
+                "What are we becoming of it now?\n" +
+                "It's fine, I said, if you're the one in my head\n" +
+                "'Cause I just don't know how to put you out\n");
+        // Pre-Chorus
+        System.out.println("'Cause I don't want another heart to fill the holes\n" +
+                "That you left under my covers for me to grow cold\n" +
+                "I don't wanna miss somebody who's not missing me\n" +
+                "But the city feels so lonely when you leave\n");
+        // Chorus
+        System.out.println("My room, where I want you\n" +
+                "Tangled ease in my sheets\n" +
+                "In your shirt that I got you\n" +
+                "Half asleep, holding onto me\n" +
+                "My bed where you left me\n" +
+                "Tangled up, now it's noon\n" +
+                "In your shirt that you gave me\n" +
+                "Half asleep, wishing I still had you\n");
+        // Outro
+        System.out.println("Ooh ooh ooh ooh ooh ooh\n" +
+                "Ooh ooh ooh ooh\n" +
+                "Ooh ooh ooh ooh ooh ooh\n" +
+                "I'm half asleep, wishing I still had you\n");
+        System.out.println("---");
+        // Einde
+    }
+}
