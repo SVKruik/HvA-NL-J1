b21b0a5069060407d3a24d14be34316afcda134d
PuffinKwadraat <sv.kruik@gmail.com>
2022-09-19 16:46:51 +0200

    Folder organisation + bsa
    Worked a little on the BSA monitor assignment. Not done yet, scope problems. Also added the Java projects to a seperate folder, since that is the logical thing to do.

--- Java/BSA-Monitor/src/Main.java
+++ Java/BSA-Monitor/src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 /**
- * Dit programma print de lyrics van een lied.
+ * Dit programma berekent je behaalde studiepunten aan het einde van het jaar.
+ * Voer je behaalde cijfers in, en check hoeveel studiepunten je hebt gehaald.
  *
  * @author Stefan Kruik
  */
@@ -40,45 +41,63 @@ public class Main {
         double rcsCijfer = scanner.nextDouble();
 
 
-
-
         if (fysCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: Fasten Your Seatbelts " + "\t\t\tCijfer: " + fysCijfer + "\t\tBehaalde punten: 0");
+            int fysBehaald = 0;
         } else if (fysCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: Fasten Your Seatbelts " + "\t\t\tCijfer: " + fysCijfer + "\t\tBehaalde punten: " + fysStudiepuntenMax);
+            int fysBehaald = fysStudiepuntenMax;
         }
 
         if (programmingCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: Programming " + "\t\t\t\tCijfer: " + programmingCijfer + "\t\tBehaalde punten: 0");
+            int programmingBehaald = 0;
         } else if (programmingCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: Programming " + "\t\t\t\tCijfer: " + programmingCijfer + "\t\tBehaalde punten: " + programmingStudiepuntenMax);
+            int programmingBehaald = programmingStudiepuntenMax;
         }
 
         if (databasesCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: Databases " + "\t\t\t\t\tCijfer: " + databasesCijfer + "\t\tBehaalde punten: 0");
+            int databasesBehaald = 0;
         } else if (databasesCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: Databases " + "\t\t\t\t\tCijfer: " + databasesCijfer + "\t\tBehaalde punten: " + databasesStudiepuntenMax);
+            int databasesBehaald = databasesStudiepuntenMax;
         }
 
         if (ppsCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: Personal and Project Skills " + "\t\tCijfer: " + ppsCijfer + "\t\tBehaalde punten: 0");
+            int ppsBehaald = 0;
         } else if (ppsCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: Personal and Project Skills " + "\t\tCijfer: " + ppsCijfer + "\t\tBehaalde punten: " + ppsStudiepuntenMax);
+            int ppsBehaald = ppsStudiepuntenMax;
         }
 
         if (uiCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: User Interaction: " + "\t\t\t\tCijfer: " + uiCijfer + "\t\tBehaalde punten: 0");
+            int uiBehaald = 0;
         } else if (uiCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: User Interaction: " + "\t\t\t\tCijfer: " + uiCijfer + "\t\tBehaalde punten: " + uiStudiepuntenMax);
+            int uiBehaald = uiStudiepuntenMax;
         }
 
         if (rcsCijfer < cijferOntoereikend) {
             System.out.println("Vak/project: Research and Communication Skills " + "\t\tCijfer: " + rcsCijfer + "\t\tBehaalde punten: 0");
+            int rcsBehaald = 0;
         } else if (rcsCijfer >= cijferOntoereikend) {
             System.out.println("Vak/project: Research and Communication Skills " + "\t\tCijfer: " + rcsCijfer + "\t\tBehaalde punten: " + rcsStudiepuntenMax);
+            int rcsBehaald = rcsStudiepuntenMax;
         }
 
-        // System.out.println("Totaal behaalde studiepunten: " behaaldeStudiepunten + "\t\t/" + totaalStudiepunten);
+        double behaaldeStudiepunten = 0;
+        
+        System.out.println("Totaal behaalde studiepunten: " + behaaldeStudiepunten + "/" + totaalStudiepunten);
+
+        double totaalStudiepuntenMinimum = totaalStudiepunten * (5/6);
+
+        if (behaaldeStudiepunten < totaalStudiepuntenMinimum) {
+            System.out.println("PAS OP: je ligt op schema voor een negatief BSA!");
+        }
 
 
         scanner.close();
