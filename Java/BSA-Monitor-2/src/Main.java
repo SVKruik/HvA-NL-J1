667a1841c6d586d8dd3442f693307475dec7b417
Stefan Kruik <stefan.kruik@hva.nl>
2022-10-06 10:05:41 +0200

    BSA Monitor 2.0

--- Java/BSA-Monitor-2/src/Main.java
+++ Java/BSA-Monitor-2/src/Main.java
@@ -1,12 +1,163 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * Een opgevoerde versie van de eerste BSA monitor.
+ * Voer je cijfers in, en hij berekent je behaalde studiepunten.
+ * Checkt ook of je op deze manier overgaat.
+ *
+ * @author Stefan Kruik
+ */
 public class Main {
+
+    public static boolean isGeldigCijfer(double vakCijfer) {
+        boolean isGeldigCijfer;
+
+        if (vakCijfer < 1 || vakCijfer > 10) {
+            isGeldigCijfer = false;
+        } else {
+            isGeldigCijfer = true;
+        }
+        return isGeldigCijfer;
+    }
+
     public static void main(String[] args) {
-        int keuze = 1;
-        switch (keuze) {
-            case 1 -> System.out.println("1");
-            case 2 -> System.out.print("2");
-            case 3 -> System.out.print("3");
-            case 4 -> System.out.print("4");
-            case 5 -> System.out.print("5");
+        Scanner scanner = new Scanner(System.in);
+
+        String[] vakNamen = {"FYS", "Programming", "UI", "Engels", "Wiskunde", "PPS"};
+        int[] vakPunten = {12, 3, 3, 1, 2, 4};
+        double[] vakCijfer = new double[6];
+
+        System.out.println("Voer behaalde cijfers in:");
+        int i;
+        for (i = 0; i < vakCijfer.length; i++) {
+            switch (i) {
+                case 0 -> {
+                    System.out.print("Fasten Your Seatbelts: ");
+                    vakCijfer[0] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[0])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("Fasten Your Seatbelts: ");
+                        vakCijfer[0] = scanner.nextDouble();
+                    }
+                }
+                case 1 -> {
+                    System.out.print("Programming: ");
+                    vakCijfer[1] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[1])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("Programming: ");
+                        vakCijfer[1] = scanner.nextDouble();
+                    }
+                }
+                case 2 -> {
+                    System.out.print("User Interaction: ");
+                    vakCijfer[2] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[2])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("User Interaction: ");
+                        vakCijfer[2] = scanner.nextDouble();
+                    }
+                }
+                case 3 -> {
+                    System.out.print("Engels: ");
+                    vakCijfer[3] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[3])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("Engels: ");
+                        vakCijfer[3] = scanner.nextDouble();
+                    }
+                }
+                case 4 -> {
+                    System.out.print("Wiskunde: ");
+                    vakCijfer[4] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[4])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("Wiskunde: ");
+                        vakCijfer[4] = scanner.nextDouble();
+                    }
+                }
+                case 5 -> {
+                    System.out.print("PPS: ");
+                    vakCijfer[5] = scanner.nextDouble();
+                    while (!isGeldigCijfer(vakCijfer[5])) {
+                        System.out.println("Ongeldig cijfer, probeer opnieuw.");
+                        System.out.print("PPS: ");
+                        vakCijfer[5] = scanner.nextDouble();
+                    }
+                }
+                default -> System.out.println("Error");
+            }
+        }
+        scanner.close();
+
+        final double VOLDOENDE = 5.5;
+
+        int fysBehaald = 0;
+        int programmingBehaald = 0;
+        int uiBehaald = 0;
+        int engelsBehaald = 0;
+        int wiskundeBehaald = 0;
+        int ppsBehaald = 0;
+
+        final int STUDIEPUNTEN_MAX = IntStream.of(vakPunten).sum();
+
+        if (vakCijfer[0] < VOLDOENDE) {
+            fysBehaald = 0;
+        } else if (vakCijfer[0] >= VOLDOENDE) {
+            fysBehaald = vakPunten[0];
+        }
+
+        if (vakCijfer[1] < VOLDOENDE) {
+            programmingBehaald = 0;
+        } else if (vakCijfer[1] >= VOLDOENDE) {
+            programmingBehaald = vakPunten[1];
+        }
+        if (vakCijfer[2] < VOLDOENDE) {
+            uiBehaald = 0;
+        } else if (vakCijfer[2] >= VOLDOENDE) {
+            uiBehaald = vakPunten[2];
+        }
+
+        if (vakCijfer[3] < VOLDOENDE) {
+            engelsBehaald = 0;
+        } else if (vakCijfer[3] >= VOLDOENDE) {
+            engelsBehaald = vakPunten[3];
+        }
+
+        if (vakCijfer[4] < VOLDOENDE) {
+            wiskundeBehaald = 0;
+        } else if (vakCijfer[4] >= VOLDOENDE) {
+            wiskundeBehaald = vakPunten[4];
+        }
+
+        if (vakCijfer[5] < VOLDOENDE) {
+            ppsBehaald = 0;
+        } else if (vakCijfer[5] >= VOLDOENDE) {
+            ppsBehaald = vakPunten[5];
+        }
+
+        System.out.println("Vak/project: Fasten Your Seatbelts " + "\t\t\t\tCijfer: " + vakCijfer[0]
+                + "\t\tBehaalde punten: " + fysBehaald);
+        System.out.println("Vak/project: Programming " + "\t\t\t\t\t\tCijfer: " + vakCijfer[1]
+                + "\t\tBehaalde punten: " + programmingBehaald);
+        System.out.println("Vak/project: User Interaction " + "\t\t\t\t\tCijfer: " + vakCijfer[2] + "\t\tBehaalde punten: "
+                + uiBehaald);
+        System.out.println("Vak/project: Engels " + "\t\t\t\t\t\t\tCijfer: " + vakCijfer[3]
+                + "\t\tBehaalde punten: " + engelsBehaald);
+        System.out.println("Vak/project: Wiskunde " + "\t\t\t\t\t\t\tCijfer: " + vakCijfer[4] + "\t\tBehaalde punten: "
+                + wiskundeBehaald);
+        System.out.println("Vak/project: Personal and Project Skills " + "\t\tCijfer: " + vakCijfer[5]
+                + "\t\tBehaalde punten: " + ppsBehaald);
+
+        int behaaldeStudiepunten = fysBehaald + programmingBehaald + uiBehaald + engelsBehaald + wiskundeBehaald + ppsBehaald;
+
+        System.out.println("Totaal behaalde studiepunten: " + behaaldeStudiepunten + "/" + STUDIEPUNTEN_MAX);
+
+        double totaalStudiepuntenMinimum = STUDIEPUNTEN_MAX / 6 * 5;
+
+        if (behaaldeStudiepunten < totaalStudiepuntenMinimum) {
+            System.out.println("PAS OP: je ligt op schema voor een negatief BSA!");
         }
     }
 }
