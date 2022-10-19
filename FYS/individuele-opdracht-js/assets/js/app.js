4a8ab2a1285ea0e1eac9a124c8ae6eb16cb1ac40
Stefan Kruik <sv.kruik@gmail.com>
2022-10-19 12:03:38 +0200

    IOJS progression

--- FYS/individuele-opdracht-js/assets/js/app.js
+++ FYS/individuele-opdracht-js/assets/js/app.js
@@ -1,9 +1,37 @@
 function myFunction() {
-    console.log("Hello world!");
-    document.querySelector(".button-text").innerHTML = "Hello world!";
+    let leeftijd = 17
+    console.log(leeftijd)
+    console.log(leeftijd.toString())
 
-    const img = document.createElement("img");
-    img.src = "./../img/Carbon.png";
-    var src = document.querySelector(".button-text")
-    src.appendChild(img);
+    const random = Math.floor(Math.random() * 6) + 1;
+    console.log(random);
+
+    console.log(`Mijn leeftijd is: ${leeftijd}.`)
+
+    const FYSTeam = ["Stefan", "Rocco", "Skip", "Joep", "Anas", "Ronin"]
+    console.log(FYSTeam.reverse());
+
+    const student = {
+        naam: "Stefan",
+        studentnummer: 50089,
+        email: "stefan.kruik@hva.nl"
+    }
+
+    console.log(student.email);
+}
+
+function myFunction2() {
+    console.log("Document loaded.")
+
+    document.getElementById("link1").innerText = href="index.html"
+    document.getElementById("link2").innerText = href="index.html"
+    document.getElementById("link3").innerText = href="index.html"
+    document.getElementById("link4").innerText = href="index.html"
+    document.getElementById("link5").innerText = href="index.html"
+}
+
+document.addEventListener('DOMContentLoaded', myFunction2);
+
+function linkAanpassen() {
+
 }
